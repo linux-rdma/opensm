42c6acfe137e8c659c9ba3613ec97abbbffec4e2
Tim Meier <meier3@llnl.gov>
2007-10-01 15:56:02 -0700

    opensm: osm_console.c added a version command
    Added a "version" comand to the console (and associated help).  It
    includes a build date, which is helpful during development.
    
    Signed-off-by: Tim Meier <meier3@llnl.gov>
    Signed-off-by: Sasha Khapyorsky <sashak@voltaire.com>

--- opensm/osm_console.c
+++ opensm/osm_console.c
@@ -844,6 +844,16 @@ static void quit_parse(char **p_last, osm_opensm_t * p_osm, FILE * out)
 	osm_console_close_socket(p_osm);
 }
 
+static void help_version(FILE *out, int detail)
+{
+  fprintf(out, "version -- print the OSM version\n");
+}
+
+static void version_parse(char **p_last, osm_opensm_t * p_osm, FILE * out)
+{
+  fprintf(out, "%s build %s %s\n", OSM_VERSION, __DATE__, __TIME__);
+}
+
 /* more parse routines go here */
 
 static const struct command console_cmds[] = {
@@ -856,6 +866,7 @@ static const struct command console_cmds[] = {
 	{"logflush", &help_logflush, &logflush_parse},
 	{"querylid", &help_querylid, &querylid_parse},
 	{"portstatus", &help_portstatus, &portstatus_parse},
+  {"version", &help_version, &version_parse},
 #ifdef ENABLE_OSM_PERF_MGR
 	{"perfmgr", &help_perfmgr, &perfmgr_parse},
 #endif				/* ENABLE_OSM_PERF_MGR */
