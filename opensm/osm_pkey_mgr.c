0ace8a836b32d6b9a3eb654566f7c83cce79b54d
Sasha Khapyorsky <sashak@voltaire.com>
2007-12-29 21:02:49 +0200

    opensm: Revert "opensm/osm_pkey_mgr.c: setting only outbound partition enforcement on switch"
    As was discussed on the list inbound partition enforcement on switch
    external port is different from end port (IBA 1.2 o18-14). This reverts
    commit eea8ce4965c401a091d1196bd38d001f92260ede.
    
    Signed-off-by: Sasha Khapyorsky <sashak@voltaire.com>

--- opensm/osm_pkey_mgr.c
+++ opensm/osm_pkey_mgr.c
@@ -212,8 +212,7 @@ pkey_mgr_enforce_partition(IN osm_log_t * p_log,
 
 	p_pi = &p_physp->port_info;
 
-	if (((p_pi->vl_enforce & 0xc) == 0x4 && enforce) ||
-	    ((p_pi->vl_enforce & 0xc) == 0 && !enforce)) {
+	if ((p_pi->vl_enforce & 0xc) == (0xc) * (enforce == TRUE)) {
 		osm_log(p_log, OSM_LOG_DEBUG,
 			"pkey_mgr_enforce_partition: "
 			"No need to update PortInfo for "
@@ -228,13 +227,10 @@ pkey_mgr_enforce_partition(IN osm_log_t * p_log,
 	memcpy(payload, p_pi, sizeof(ib_port_info_t));
 
 	p_pi = (ib_port_info_t *) payload;
-
-	/* clearing enforcement in both directions */
-	p_pi->vl_enforce &= ~0xc;
 	if (enforce == TRUE)
-		/* enforcing only in outbound direction */
-		p_pi->vl_enforce |= 0x4;
-
+		p_pi->vl_enforce |= 0xc;
+	else
+		p_pi->vl_enforce &= ~0xc;
 	p_pi->state_info2 = 0;
 	ib_port_info_set_port_state(p_pi, IB_LINK_NO_CHANGE);
 
