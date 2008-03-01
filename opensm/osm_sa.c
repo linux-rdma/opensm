3daabe950db11615cd9e9690df3fc2de2bd8b3ef
Sasha Khapyorsky <sashak@voltaire.com>
2008-03-01 22:21:09 +0200

    opensm: move osm_sa_send_error() to osm_sa.c file
    Move osm_sa_send_error() tp osm_sa.c file. Remove empty
    osm_sa_response.c file.
    
    Signed-off-by: Sasha Khapyorsky <sashak@voltaire.com>

--- opensm/osm_sa.c
+++ opensm/osm_sa.c
@@ -68,6 +68,7 @@
 #include <opensm/osm_multicast.h>
 #include <opensm/osm_inform.h>
 #include <opensm/osm_service.h>
+#include <opensm/osm_helper.h>
 #include <vendor/osm_vendor_api.h>
 
 #define  OSM_SA_INITIAL_TID_VALUE 0xabc
@@ -334,6 +335,68 @@ osm_sa_vendor_send(IN osm_bind_handle_t h_bind,
 			ib_get_err_str(status));
 	}
 	return status;
+}
+
+void
+osm_sa_send_error(IN osm_sa_t * sa,
+		  IN const osm_madw_t * const p_madw,
+		  IN const ib_net16_t sa_status)
+{
+	osm_madw_t *p_resp_madw;
+	ib_sa_mad_t *p_resp_sa_mad;
+	ib_sa_mad_t *p_sa_mad;
+
+	OSM_LOG_ENTER(sa->p_log);
+
+	/* avoid races - if we are exiting - exit */
+	if (osm_exit_flag) {
+		OSM_LOG(sa->p_log, OSM_LOG_DEBUG,
+			"Ignoring requested send after exit\n");
+		goto Exit;
+	}
+
+	p_resp_madw = osm_mad_pool_get(sa->p_mad_pool,
+				       p_madw->h_bind, MAD_BLOCK_SIZE,
+				       &p_madw->mad_addr);
+
+	if (p_resp_madw == NULL) {
+		OSM_LOG(sa->p_log, OSM_LOG_ERROR, "ERR 2301: "
+			"Unable to acquire response MAD\n");
+		goto Exit;
+	}
+
+	p_resp_sa_mad = osm_madw_get_sa_mad_ptr(p_resp_madw);
+	p_sa_mad = osm_madw_get_sa_mad_ptr(p_madw);
+
+	/*  Copy the MAD header back into the response mad */
+	*p_resp_sa_mad = *p_sa_mad;
+	p_resp_sa_mad->status = sa_status;
+
+	if (p_resp_sa_mad->method == IB_MAD_METHOD_SET)
+		p_resp_sa_mad->method = IB_MAD_METHOD_GET;
+
+	p_resp_sa_mad->method |= IB_MAD_METHOD_RESP_MASK;
+
+	/*
+	 * C15-0.1.5 - always return SM_Key = 0 (table 185 p 884)
+	 */
+	p_resp_sa_mad->sm_key = 0;
+
+	/*
+	 * o15-0.2.7 - The PathRecord Attribute ID shall be used in
+	 * the response (to a SubnAdmGetMulti(MultiPathRecord)
+	 */
+	if (p_resp_sa_mad->attr_id == IB_MAD_ATTR_MULTIPATH_RECORD)
+		p_resp_sa_mad->attr_id = IB_MAD_ATTR_PATH_RECORD;
+
+	if (osm_log_is_active(sa->p_log, OSM_LOG_FRAMES))
+		osm_dump_sa_mad(sa->p_log, p_resp_sa_mad, OSM_LOG_FRAMES);
+
+	osm_sa_vendor_send(osm_madw_get_bind_handle(p_resp_madw),
+			   p_resp_madw, FALSE, sa->p_subn);
+
+Exit:
+	OSM_LOG_EXIT(sa->p_log);
 }
 
 /**********************************************************************
