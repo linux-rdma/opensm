065cdf639e7b80eef73c6d7ff4b734061b03b6d1
Sasha Khapyorsky <sashak@voltaire.com>
2008-06-16 08:30:05 +0300

    opensm/osm_console: use osm_get_switch_by_guid()
    Use osm_get_switch_by_guid() function instead of direct cl_qmap_get().
    
    Signed-off-by: Sasha Khapyorsky <sashak@voltaire.com>

--- opensm/osm_console.c
+++ opensm/osm_console.c
@@ -815,7 +815,6 @@ static void switchbalance_parse(char **p_last, osm_opensm_t * p_osm, FILE * out)
 	char *p_cmd;
 	uint64_t guid = 0;
 	osm_switch_t *p_sw;
-	cl_qmap_t *p_sw_guid_tbl;
 	int verbose = 0;
 
 	p_cmd = next_token(p_last);
@@ -838,18 +837,16 @@ static void switchbalance_parse(char **p_last, osm_opensm_t * p_osm, FILE * out)
 	}
 
 	cl_plock_acquire(&p_osm->lock);
-	p_sw_guid_tbl = &p_osm->subn.sw_guid_tbl;
 	if (guid) {
-		p_sw =
-		    (osm_switch_t *) cl_qmap_get(p_sw_guid_tbl,
-						 cl_hton64(guid));
-		if (p_sw == (osm_switch_t *) cl_qmap_end(p_sw_guid_tbl)) {
+		p_sw = osm_get_switch_by_guid(&p_osm->subn, cl_hton64(guid));
+		if (!p_sw) {
 			fprintf(out, "guid not found\n");
 			goto lock_exit;
 		}
 
 		switchbalance_check(p_osm, p_sw, out, verbose);
 	} else {
+		cl_qmap_t *p_sw_guid_tbl = &p_osm->subn.sw_guid_tbl;
 		for (p_sw = (osm_switch_t *) cl_qmap_head(p_sw_guid_tbl);
 		     p_sw != (osm_switch_t *) cl_qmap_end(p_sw_guid_tbl);
 		     p_sw = (osm_switch_t *) cl_qmap_next(&p_sw->map_item))
@@ -1023,16 +1020,15 @@ static void lidbalance_check(osm_opensm_t * p_osm,
 static void lidbalance_parse(char **p_last, osm_opensm_t * p_osm, FILE * out)
 {
 	char *p_cmd;
-	uint64_t switchguid = 0;
+	uint64_t guid = 0;
 	osm_switch_t *p_sw;
-	cl_qmap_t *p_sw_guid_tbl;
 
 	p_cmd = next_token(p_last);
 	if (p_cmd) {
 		char *p_end;
 
-		switchguid = strtoull(p_cmd, &p_end, 0);
-		if (!switchguid || *p_end != '\0') {
+		guid = strtoull(p_cmd, &p_end, 0);
+		if (!guid || *p_end != '\0') {
 			fprintf(out, "Invalid switchguid specified\n");
 			help_lidbalance(out, 1);
 			return;
@@ -1040,18 +1036,15 @@ static void lidbalance_parse(char **p_last, osm_opensm_t * p_osm, FILE * out)
 	}
 
 	cl_plock_acquire(&p_osm->lock);
-	p_sw_guid_tbl = &p_osm->subn.sw_guid_tbl;
-	if (switchguid) {
-		p_sw =
-		    (osm_switch_t *) cl_qmap_get(p_sw_guid_tbl,
-						 cl_hton64(switchguid));
-		if (p_sw == (osm_switch_t *) cl_qmap_end(p_sw_guid_tbl)) {
+	if (guid) {
+		p_sw = osm_get_switch_by_guid(&p_osm->subn, cl_hton64(guid));
+		if (!p_sw) {
 			fprintf(out, "switchguid not found\n");
 			goto lock_exit;
 		}
-
 		lidbalance_check(p_osm, p_sw, out);
 	} else {
+		cl_qmap_t *p_sw_guid_tbl = &p_osm->subn.sw_guid_tbl;
 		for (p_sw = (osm_switch_t *) cl_qmap_head(p_sw_guid_tbl);
 		     p_sw != (osm_switch_t *) cl_qmap_end(p_sw_guid_tbl);
 		     p_sw = (osm_switch_t *) cl_qmap_next(&p_sw->map_item))
