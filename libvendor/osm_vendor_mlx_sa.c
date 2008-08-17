d4906128449dc3f321f2f9cb654d82827b37c14d
Yevgeny Kliteynik <kliteyn@dev.mellanox.co.il>
2008-08-17 18:12:18 +0300

    opensm/libvendor/osm_vendor_mlx_sa.c: handling attribute offset of 0
    Following patch f752a876edecfdac9e1d07bc0747a2c296eed230,
    "set SA attribute offset to 0 when no records are returned"
    
    Attribute offset of 0 caused the vendor to crash.
    
    Signed-off-by: Yevgeny Kliteynik <kliteyn@dev.mellanox.co.il>
    Signed-off-by: Sasha Khapyorsky <sashak@voltaire.com>

--- libvendor/osm_vendor_mlx_sa.c
+++ libvendor/osm_vendor_mlx_sa.c
@@ -140,18 +140,24 @@ __osmv_sa_mad_rcv_cb(IN osm_madw_t * p_madw,
 #else
 			/* we used the offset value to calculate the number of
 			   records in here */
-			query_res.result_cnt = (uintn_t)
-			    ((p_madw->mad_size - IB_SA_MAD_HDR_SIZE) /
-			     ib_get_attr_size(p_sa_mad->attr_offset));
-			osm_log(p_bind->p_log, OSM_LOG_DEBUG,
-				"__osmv_sa_mad_rcv_cb: Count = %u = %u / %u (%u)\n",
-				query_res.result_cnt,
-				p_madw->mad_size - IB_SA_MAD_HDR_SIZE,
-				ib_get_attr_size(p_sa_mad->attr_offset),
-				(p_madw->mad_size -
-				 IB_SA_MAD_HDR_SIZE) %
-				ib_get_attr_size(p_sa_mad->attr_offset)
-			    );
+			if (ib_get_attr_size(p_sa_mad->attr_offset) == 0) {
+				query_res.result_cnt = 0;
+				osm_log(p_bind->p_log, OSM_LOG_DEBUG,
+					"__osmv_sa_mad_rcv_cb: Count = 0\n");
+			}
+			else {
+				query_res.result_cnt = (uintn_t)
+					((p_madw->mad_size - IB_SA_MAD_HDR_SIZE) /
+					 ib_get_attr_size(p_sa_mad->attr_offset));
+				osm_log(p_bind->p_log, OSM_LOG_DEBUG,
+					"__osmv_sa_mad_rcv_cb: "
+					"Count = %u = %zu / %u (%zu)\n",
+					query_res.result_cnt,
+					p_madw->mad_size - IB_SA_MAD_HDR_SIZE,
+					ib_get_attr_size(p_sa_mad->attr_offset),
+					(p_madw->mad_size - IB_SA_MAD_HDR_SIZE) %
+					ib_get_attr_size(p_sa_mad->attr_offset));
+			}
 #endif
 		}
 	}
