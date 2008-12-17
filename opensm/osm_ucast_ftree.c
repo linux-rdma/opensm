ee3953d86cf15edba3563c767e9108d0b84254dd
Yevgeny Kliteynik <kliteyn@dev.mellanox.co.il>
2008-12-17 17:00:54 +0200

    opensm/osm_ucast_ftree.c: fixing errors in comments
    Hi Sasha,
    
    Following Nicolas' fix, here's some fixes in the fat-tree routing comments.
    
    Signed-off-by: Yevgeny Kliteynik <kliteyn@dev.mellanox.co.il>
    Signed-off-by: Sasha Khapyorsky <sashak@voltaire.com>

--- opensm/osm_ucast_ftree.c
+++ opensm/osm_ucast_ftree.c
@@ -2462,11 +2462,11 @@ static void __osm_ftree_fabric_route_to_cns(IN ftree_fabric_t * p_ftree)
  *    foreach HCA non-CN port in fabric
  *       obtain the LID of the HCA port
  *       get switch that is connected to this HCA port
- *       set switch LFT(LID) to the port connecting to compute node
- *       call assign-down-going-port-by-ascending-up(TRUE,TRUE) on CURRENT switch
+ *       set switch LFT(LID) to the port connected to the HCA port
+ *       call assign-down-going-port-by-ascending-up(TRUE,TRUE) on the switch
  *
- * Routing to these HCAs is routing a REAL hca lid on SECONDARY path.
- * However, we do want to allow load-leveling of the traffic to the non-CNs,
+ * Routing to these HCAs is routing a REAL hca lid on MAIN path.
+ * We want to allow load-leveling of the traffic to the non-CNs,
  * because such nodes may include IO nodes with heavy usage
  *   - we should set fwd tables
  *   - we should update port counters
