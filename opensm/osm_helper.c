3e7a84cf774ba7291ee7a4655e8a6b03099afb1d
Sasha Khapyorsky <sashak@voltaire.com>
2009-04-14 21:11:36 +0300

    opensm/osm_helper.c: use single buffer in osm_dump_dr_smp()
    Use single dump buffer in osm_dump_dr_smp() function, prevent data
    copying.
    
    Signed-off-by: Sasha Khapyorsky <sashak@voltaire.com>

--- opensm/osm_helper.c
+++ opensm/osm_helper.c
@@ -1901,104 +1901,101 @@ void osm_dump_dr_smp(IN osm_log_t * p_log, IN const ib_smp_t * p_smp,
 		     IN const osm_log_level_t log_level)
 {
 	if (osm_log_is_active(p_log, log_level)) {
-		char buf[BUF_SIZE], line[BUF_SIZE];
-		uint32_t i;
+		char buf[BUF_SIZE];
+		unsigned n;
 
-		sprintf(buf,
-			"SMP dump:\n"
-			"\t\t\t\tbase_ver................0x%X\n"
-			"\t\t\t\tmgmt_class..............0x%X\n"
-			"\t\t\t\tclass_ver...............0x%X\n"
-			"\t\t\t\tmethod..................0x%X (%s)\n",
-			p_smp->base_ver,
-			p_smp->mgmt_class,
-			p_smp->class_ver,
-			p_smp->method, ib_get_sm_method_str(p_smp->method));
+		n = sprintf(buf,
+			    "SMP dump:\n"
+			    "\t\t\t\tbase_ver................0x%X\n"
+			    "\t\t\t\tmgmt_class..............0x%X\n"
+			    "\t\t\t\tclass_ver...............0x%X\n"
+			    "\t\t\t\tmethod..................0x%X (%s)\n",
+			    p_smp->base_ver, p_smp->mgmt_class,
+			    p_smp->class_ver, p_smp->method,
+			    ib_get_sm_method_str(p_smp->method));
 
 		if (p_smp->mgmt_class == IB_MCLASS_SUBN_DIR) {
-			sprintf(line,
-				"\t\t\t\tD bit...................0x%X\n"
-				"\t\t\t\tstatus..................0x%X\n",
-				ib_smp_is_d(p_smp), ib_smp_get_status(p_smp));
+			n += snprintf(buf + n, sizeof(buf) - n,
+				      "\t\t\t\tD bit...................0x%X\n"
+				      "\t\t\t\tstatus..................0x%X\n",
+				      ib_smp_is_d(p_smp),
+				      ib_smp_get_status(p_smp));
 		} else {
-			sprintf(line,
-				"\t\t\t\tstatus..................0x%X\n",
-				cl_ntoh16(p_smp->status));
+			n += snprintf(buf + n, sizeof(buf) - n,
+				      "\t\t\t\tstatus..................0x%X\n",
+				      cl_ntoh16(p_smp->status));
 		}
-		strcat(buf, line);
 
-		sprintf(line,
-			"\t\t\t\thop_ptr.................0x%X\n"
-			"\t\t\t\thop_count...............0x%X\n"
-			"\t\t\t\ttrans_id................0x%" PRIx64 "\n"
-			"\t\t\t\tattr_id.................0x%X (%s)\n"
-			"\t\t\t\tresv....................0x%X\n"
-			"\t\t\t\tattr_mod................0x%X\n"
-			"\t\t\t\tm_key...................0x%016" PRIx64 "\n",
-			p_smp->hop_ptr,
-			p_smp->hop_count,
-			cl_ntoh64(p_smp->trans_id),
-			cl_ntoh16(p_smp->attr_id),
-			ib_get_sm_attr_str(p_smp->attr_id),
-			cl_ntoh16(p_smp->resv),
-			cl_ntoh32(p_smp->attr_mod), cl_ntoh64(p_smp->m_key));
-		strcat(buf, line);
+		n += snprintf(buf + n, sizeof(buf) - n,
+			      "\t\t\t\thop_ptr.................0x%X\n"
+			      "\t\t\t\thop_count...............0x%X\n"
+			      "\t\t\t\ttrans_id................0x%" PRIx64 "\n"
+			      "\t\t\t\tattr_id.................0x%X (%s)\n"
+			      "\t\t\t\tresv....................0x%X\n"
+			      "\t\t\t\tattr_mod................0x%X\n"
+			      "\t\t\t\tm_key...................0x%016" PRIx64
+			      "\n", p_smp->hop_ptr, p_smp->hop_count,
+			      cl_ntoh64(p_smp->trans_id),
+			      cl_ntoh16(p_smp->attr_id),
+			      ib_get_sm_attr_str(p_smp->attr_id),
+			      cl_ntoh16(p_smp->resv),
+			      cl_ntoh32(p_smp->attr_mod),
+			      cl_ntoh64(p_smp->m_key));
 
 		if (p_smp->mgmt_class == IB_MCLASS_SUBN_DIR) {
-			sprintf(line,
-				"\t\t\t\tdr_slid.................%u\n"
-				"\t\t\t\tdr_dlid.................%u\n",
-				cl_ntoh16(p_smp->dr_slid),
-				cl_ntoh16(p_smp->dr_dlid));
-			strcat(buf, line);
+			uint32_t i;
+			n += snprintf(buf + n, sizeof(buf) - n,
+				      "\t\t\t\tdr_slid.................%u\n"
+				      "\t\t\t\tdr_dlid.................%u\n",
+				      cl_ntoh16(p_smp->dr_slid),
+				      cl_ntoh16(p_smp->dr_dlid));
 
-			strcat(buf, "\n\t\t\t\tInitial path: ");
-			sprint_uint8_arr(line, sizeof(line),
-					 p_smp->initial_path,
-					 p_smp->hop_count + 1);
-			strcat(buf, line);
+			n += snprintf(buf + n, sizeof(buf) - n,
+				      "\n\t\t\t\tInitial path: ");
+			n += sprint_uint8_arr(buf + n, sizeof(buf) - n,
+					      p_smp->initial_path,
+					      p_smp->hop_count + 1);
 
-			strcat(buf, "\n\t\t\t\tReturn path:  ");
-			sprint_uint8_arr(line, sizeof(line),
-					 p_smp->return_path,
-					 p_smp->hop_count + 1);
-			strcat(buf, line);
+			n += snprintf(buf + n, sizeof(buf) - n,
+				      "\n\t\t\t\tReturn path:  ");
+			n += sprint_uint8_arr(buf + n, sizeof(buf) - n,
+					      p_smp->return_path,
+					      p_smp->hop_count + 1);
 
-			strcat(buf, "\n\t\t\t\tReserved:     ");
-
+			n += snprintf(buf + n, sizeof(buf) - n,
+				      "\n\t\t\t\tReserved:     ");
 			for (i = 0; i < 7; i++) {
-				sprintf(line, "[%0X]", p_smp->resv1[i]);
-				strcat(buf, line);
+				n += snprintf(buf + n, sizeof(buf) - n,
+					      "[%0X]", p_smp->resv1[i]);
 			}
-
-			strcat(buf, "\n");
+			n += snprintf(buf + n, sizeof(buf) - n, "\n");
 
 			for (i = 0; i < 64; i += 16) {
-				sprintf(line, "\n\t\t\t\t%02X %02X %02X %02X "
-					"%02X %02X %02X %02X"
-					"   %02X %02X %02X %02X %02X %02X %02X %02X\n",
-					p_smp->data[i],
-					p_smp->data[i + 1],
-					p_smp->data[i + 2],
-					p_smp->data[i + 3],
-					p_smp->data[i + 4],
-					p_smp->data[i + 5],
-					p_smp->data[i + 6],
-					p_smp->data[i + 7],
-					p_smp->data[i + 8],
-					p_smp->data[i + 9],
-					p_smp->data[i + 10],
-					p_smp->data[i + 11],
-					p_smp->data[i + 12],
-					p_smp->data[i + 13],
-					p_smp->data[i + 14],
-					p_smp->data[i + 15]);
-
-				strcat(buf, line);
+				n += snprintf(buf + n, sizeof(buf) - n,
+					      "\n\t\t\t\t%02X %02X %02X %02X "
+					      "%02X %02X %02X %02X"
+					      "   %02X %02X %02X %02X %02X %02X %02X %02X\n",
+					      p_smp->data[i],
+					      p_smp->data[i + 1],
+					      p_smp->data[i + 2],
+					      p_smp->data[i + 3],
+					      p_smp->data[i + 4],
+					      p_smp->data[i + 5],
+					      p_smp->data[i + 6],
+					      p_smp->data[i + 7],
+					      p_smp->data[i + 8],
+					      p_smp->data[i + 9],
+					      p_smp->data[i + 10],
+					      p_smp->data[i + 11],
+					      p_smp->data[i + 12],
+					      p_smp->data[i + 13],
+					      p_smp->data[i + 14],
+					      p_smp->data[i + 15]);
 			}
 		} else {
 			/* not a Direct Route so provide source and destination lids */
-			strcat(buf, "\t\t\t\tMAD IS LID ROUTED\n");
+			n += snprintf(buf + n, sizeof(buf) - n,
+				      "\t\t\t\tMAD IS LID ROUTED\n");
 		}
 
 		osm_log(p_log, log_level, "%s\n", buf);
