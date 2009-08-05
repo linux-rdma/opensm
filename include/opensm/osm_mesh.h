616a1c317d1a447ad2fa9bffe741a0fa646e76b6
Hal Rosenstock <hnrose@comcast.net>
2009-08-05 15:03:44 -0400

    opensm/osm_mesh.h: Fix SFW copyright
    Signed-off-by: Hal Rosenstock <hal.rosenstock@gmail.com>
    Signed-off-by: Sasha Khapyorsky <sashak@voltaire.com>

--- include/opensm/osm_mesh.h
+++ include/opensm/osm_mesh.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2088      System Fabric Works, Inc.
+ * Copyright (c) 2008,2009  System Fabric Works, Inc. All rights reserved.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
