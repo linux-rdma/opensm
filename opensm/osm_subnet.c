1127cc33d5f2f5f30ffc005433f41c84018b0cdf
Sasha Khapyorsky <sashak@voltaire.com>
2009-09-23 18:40:31 +0300

    opensm/osm_get_port_by_lid(): don't bother with lmc
    Since subn->port_lid_tbl vector is filled for all port's LIDs in
    accordance with its LMC value, so we don't need to bother with LMC
    tracking and instead can just return a pointer indexed by requested lid.
    Obviously it speeds this helper up significantly.
    
    Signed-off-by: Sasha Khapyorsky <sashak@voltaire.com>

--- opensm/osm_subnet.c
+++ opensm/osm_subnet.c
@@ -645,26 +645,9 @@ osm_port_t *osm_get_port_by_guid(IN osm_subn_t const *p_subn, IN ib_net64_t guid
  **********************************************************************/
 osm_port_t *osm_get_port_by_lid(IN osm_subn_t const * subn, IN ib_net16_t lid)
 {
-	osm_port_t *port = NULL;
-	uint16_t base_lid;
-	uint8_t lmc;
-
 	lid = cl_ntoh16(lid);
-	if (lid >= cl_ptr_vector_get_size(&subn->port_lid_tbl))
-		return NULL;
-
-	/* Loop on lmc from 0 up through max LMC possible */
-	for (lmc = 0; lmc <= IB_PORT_LMC_MAX; lmc++) {
-		/* Calculate a base LID assuming this is the real LMC */
-		base_lid = lid & ~((1 << lmc) - 1);
-
-		port = cl_ptr_vector_get(&subn->port_lid_tbl, base_lid);
-		/* Determine if base LID "tested" is the real base LID */
-		/* This is true if the LMC "tested" is the port's actual LMC */
-		if (port && lmc <= osm_port_get_lmc(port))
-			return port;
-	}
-
+	if (lid < cl_ptr_vector_get_size(&subn->port_lid_tbl))
+		return cl_ptr_vector_get(&subn->port_lid_tbl, lid);
 	return NULL;
 }
 
