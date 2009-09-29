10cf6ecf9fe30495e5a3018ed1cff008fac93e71
Hal Rosenstock <hnrose@comcast.net>
2009-09-29 17:55:40 -0400

    opensm/osm_mesh.c: Add dump_mesh routine at OSM_LOG_DEBUG level
    Signed-off-by: Hal Rosenstock <hal.rosenstock@gmail.com>
    Signed-off-by: Sasha Khapyorsky <sashak@voltaire.com>

--- opensm/osm_mesh.c
+++ opensm/osm_mesh.c
@@ -1561,6 +1561,63 @@ err:
 	return -1;
 }
 
+static void dump_mesh(lash_t *p_lash)
+{
+	osm_log_t *p_log = &p_lash->p_osm->log;
+	int sw;
+	int num_switches = p_lash->num_switches;
+	int dimension;
+	int i, j, k, n;
+	switch_t *s, *s2;
+	char buf[256];
+
+	OSM_LOG_ENTER(p_log);
+
+	for (sw = 0; sw < num_switches; sw++) {
+		s = p_lash->switches[sw];
+		dimension = s->node->dimension;
+		n = sprintf(buf, "[");
+		for (i = 0; i < dimension; i++) {
+			n += snprintf(buf + n, sizeof(buf) - n,
+				      "%2d", s->node->coord[i]);
+			if (n > sizeof(buf))
+				n = sizeof(buf);
+			if (i != dimension - 1) {
+				n += snprintf(buf + n, sizeof(buf) - n, "%s", ",");
+				if (n > sizeof(buf))
+					n = sizeof(buf);
+			}
+		}
+		n += snprintf(buf + n, sizeof(buf) - n, "]");
+		if (n > sizeof(buf))
+			n = sizeof(buf);
+		for (j = 0; j < s->node->num_links; j++) {
+			s2 = p_lash->switches[s->node->links[j]->switch_id];
+			n += snprintf(buf + n, sizeof(buf) - n, " [%d]->[", j);
+			if (n > sizeof(buf))
+				n = sizeof(buf);
+			for (k = 0; k < dimension; k++) {
+				n += snprintf(buf + n, sizeof(buf) - n, "%2d",
+					      s2->node->coord[k]);
+				if (n > sizeof(buf))
+					n = sizeof(buf);
+				if (k != dimension - 1) {
+					n += snprintf(buf + n, sizeof(buf) - n,
+						      ",");
+					if (n > sizeof(buf))
+						n = sizeof(buf);
+				}
+			}
+			n += snprintf(buf + n, sizeof(buf) - n, "]");
+			if (n > sizeof(buf))
+				n = sizeof(buf);
+		}
+		OSM_LOG(p_log, OSM_LOG_DEBUG, "%s\n", buf);
+	}
+
+	OSM_LOG_EXIT(p_log);
+}
+
 /*
  * osm_do_mesh_analysis
  */
@@ -1648,6 +1705,9 @@ int osm_do_mesh_analysis(lash_t *p_lash)
 
 		OSM_LOG(p_log, OSM_LOG_INFO, "%s", buf);
 	}
+
+	if (osm_log_is_active(p_log, OSM_LOG_DEBUG))
+		dump_mesh(p_lash);
 
 done:
 	mesh_delete(mesh);
