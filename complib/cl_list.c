112c97a1d80372b71e0fd9cef1c6bb12f680c472
Yevgeny Kliteynik <kliteyn@dev.mellanox.co.il>
2009-11-08 15:28:16 +0200

    opensm/complib: bug in cl_list_insert_array_head/tail functions
    Fixing bug in cl_list_insert_array_head/tail functions.
    In case of failure, no cleanup is done.
    
    Signed-off-by: Yevgeny Kliteynik <kliteyn@dev.mellanox.co.il>
    Signed-off-by: Sasha Khapyorsky <sashak@voltaire.com>

--- complib/cl_list.c
+++ complib/cl_list.c
@@ -424,6 +424,7 @@ cl_status_t cl_list_insert_array_head(IN cl_list_t * const p_list,
 {
 	cl_status_t status;
 	void *p_object;
+	uint32_t items_remain = item_count;
 
 	CL_ASSERT(p_list);
 	CL_ASSERT(cl_is_qpool_inited(&p_list->list_item_pool));
@@ -439,11 +440,11 @@ cl_status_t cl_list_insert_array_head(IN cl_list_t * const p_list,
 	p_object = ((uint8_t *) p_array + (item_size * (item_count - 1)));
 
 	/* Continue to add all items to the list. */
-	while (item_count--) {
+	while (items_remain--) {
 		status = cl_list_insert_head(p_list, p_object);
 		if (status != CL_SUCCESS) {
 			/* Remove all items that have been inserted. */
-			while (item_count++ < item_count)
+			while (items_remain++ < (item_count - 1))
 				cl_list_remove_head(p_list);
 			return (status);
 		}
@@ -462,6 +463,7 @@ cl_status_t cl_list_insert_array_tail(IN cl_list_t * const p_list,
 {
 	cl_status_t status;
 	void *p_object;
+	uint32_t items_remain = item_count;
 
 	CL_ASSERT(p_list);
 	CL_ASSERT(cl_is_qpool_inited(&p_list->list_item_pool));
@@ -473,11 +475,11 @@ cl_status_t cl_list_insert_array_tail(IN cl_list_t * const p_list,
 	p_object = (void *)p_array;
 
 	/* Continue to add all items to the list. */
-	while (item_count--) {
+	while (items_remain--) {
 		status = cl_list_insert_tail(p_list, p_object);
 		if (status != CL_SUCCESS) {
 			/* Remove all items that have been inserted. */
-			while (item_count++ < item_count)
+			while (items_remain++ < (item_count - 1))
 				cl_list_remove_tail(p_list);
 			return (status);
 		}
