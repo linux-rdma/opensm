d69a82c2109a0d791a40d872c41ed721b84f6157
Hal Rosenstock <hal@mellanox.com>
2011-02-02 18:43:43 +0200

    opensm: Add support for SwitchInfo:MulticastFDBTop
    Added by MgtWG errata #4505-4508
    Also, per MgtWG RefID #4640, MulticastFDBTop value of 0xbfff means no entries
    
    In osm_sm.c:osm_sm_set_mcast_tbl, when switch port 0 indicates
    IsMulticastFDBTop supported, set MulticastFDBTop in SwitchInfo
    based on max_block_in_use
    
    Signed-off-by: Hal Rosenstock <hal.rosenstock@gmail.com>
    Signed-off-by: Yevgeny Kliteynik <kliteyn@dev.mellanox.co.il>

--- opensm/osm_mcast_mgr.c
+++ opensm/osm_mcast_mgr.c
@@ -1017,6 +1017,63 @@ static ib_api_status_t mcast_mgr_process_mlid(osm_sm_t * sm, uint16_t mlid)
 	return status;
 }
 
+static void mcast_mgr_set_mfttop(IN osm_sm_t * sm, IN osm_switch_t * p_sw)
+{
+	osm_node_t *p_node;
+	osm_dr_path_t *p_path;
+	osm_physp_t *p_physp;
+	osm_mcast_tbl_t *p_tbl;
+	osm_madw_context_t context;
+	ib_api_status_t status;
+	ib_switch_info_t si;
+	uint16_t mcast_top;
+
+	OSM_LOG_ENTER(sm->p_log);
+
+	CL_ASSERT(p_sw);
+
+	p_node = p_sw->p_node;
+
+	CL_ASSERT(p_node);
+
+	p_physp = osm_node_get_physp_ptr(p_node, 0);
+	p_path = osm_physp_get_dr_path_ptr(p_physp);
+	p_tbl = osm_switch_get_mcast_tbl_ptr(p_sw);
+
+	if (p_physp->port_info.capability_mask & IB_PORT_CAP_HAS_MCAST_FDB_TOP) {
+		/*
+		   Set the top of the multicast forwarding table.
+		 */
+		si = p_sw->switch_info;
+		if (p_tbl->max_block_in_use == -1)
+			mcast_top = cl_hton16(IB_LID_MCAST_START_HO - 1);
+		else
+			mcast_top = cl_hton16(IB_LID_MCAST_START_HO +
+					      (p_tbl->max_block_in_use + 1) * IB_MCAST_BLOCK_SIZE - 1);
+		if (mcast_top == si.mcast_top)
+			return;
+
+		si.mcast_top = mcast_top;
+
+		OSM_LOG(sm->p_log, OSM_LOG_DEBUG,
+			"Setting switch MFT top to MLID 0x%x\n",
+			cl_ntoh16(si.mcast_top));
+
+		context.si_context.light_sweep = FALSE;
+		context.si_context.node_guid = osm_node_get_node_guid(p_node);
+		context.si_context.set_method = TRUE;
+
+		status = osm_req_set(sm, p_path, (uint8_t *) & si,
+				     sizeof(si), IB_MAD_ATTR_SWITCH_INFO,
+				     0, CL_DISP_MSGID_NONE, &context);
+
+		if (status != IB_SUCCESS)
+			OSM_LOG(sm->p_log, OSM_LOG_ERROR, "ERR 0A1B: "
+				"Sending SwitchInfo attribute failed (%s)\n",
+				ib_get_err_str(status));
+	}
+}
+
 static int mcast_mgr_set_mftables(osm_sm_t * sm)
 {
 	cl_qmap_t *p_sw_tbl = &sm->p_subn->sw_guid_tbl;
@@ -1032,6 +1089,7 @@ static int mcast_mgr_set_mftables(osm_sm_t * sm)
 		p_tbl = osm_switch_get_mcast_tbl_ptr(p_sw);
 		if (osm_mcast_tbl_get_max_block_in_use(p_tbl) > max_block)
 			max_block = osm_mcast_tbl_get_max_block_in_use(p_tbl);
+		mcast_mgr_set_mfttop(sm, p_sw);
 		p_sw = (osm_switch_t *) cl_qmap_next(&p_sw->map_item);
 	}
 
