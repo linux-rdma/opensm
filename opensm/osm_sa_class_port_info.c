a7e9436bd1d6ce72896f53eb200563c0f4e94dcf
Hal Rosenstock <hal@mellanox.com>
2011-02-02 18:43:43 +0200

    opensm: Add support for SwitchInfo:MulticastFDBTop
    Added by MgtWG errata #4505-4508
    Also, per MgtWG RefID #4640, MulticastFDBTop value of 0xbfff means no entries
    
    In osm_sm.c:osm_sm_set_mcast_tbl, when switch port 0 indicates
    IsMulticastFDBTop supported, set MulticastFDBTop in SwitchInfo
    based on max_block_in_use
    
    Signed-off-by: Hal Rosenstock <hal.rosenstock@gmail.com>
    Signed-off-by: Yevgeny Kliteynik <kliteyn@dev.mellanox.co.il>

--- opensm/osm_sa_class_port_info.c
+++ opensm/osm_sa_class_port_info.c
@@ -157,7 +157,10 @@ static void cpi_rcv_respond(IN osm_sa_t * sa, IN const osm_madw_t * p_madw)
 	    OSM_CAP_IS_PORT_INFO_CAPMASK_MATCH_SUPPORTED;
 #endif
 	if (sa->p_subn->opt.qos)
-		ib_class_set_cap_mask2(p_resp_cpi, OSM_CAP2_IS_QOS_SUPPORTED);
+		ib_class_set_cap_mask2(p_resp_cpi, OSM_CAP2_IS_QOS_SUPPORTED |
+						   OSM_CAP2_IS_MCAST_TOP_SUPPORTED);
+	else
+		ib_class_set_cap_mask2(p_resp_cpi, OSM_CAP2_IS_MCAST_TOP_SUPPORTED);
 
 	if (!sa->p_subn->opt.disable_multicast)
 		p_resp_cpi->cap_mask |= OSM_CAP_IS_UD_MCAST_SUP;
