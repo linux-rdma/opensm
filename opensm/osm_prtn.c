570715833b23f5343c06b43843ae5bf2f3dfe72a
Hal Rosenstock <hal@mellanox.com>
2011-04-04 13:32:36 +0300

    opensm/osm_prtn.c: Cosmetic formatting change
    Signed-off-by: Hal Rosenstock <hal@mellanox.com>
    Signed-off-by: Alex Netes <alexne@mellanox.com>

--- opensm/osm_prtn.c
+++ opensm/osm_prtn.c
@@ -253,7 +253,6 @@ osm_prtn_t *osm_prtn_make_new(osm_log_t * p_log, osm_subn_t * p_subn,
 	osm_prtn_t *p = NULL, *p_check;
 
 	pkey &= cl_hton16((uint16_t) ~ 0x8000);
-
 	if (!pkey) {
 		if (name && (p = osm_prtn_find_by_name(p_subn, name)))
 			return p;
