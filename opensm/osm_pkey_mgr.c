77175057c673e2fca6cf8064faf8f5465f8e9479
Daniel Klein <danielk@mellanox.com>
2011-11-30 10:09:04 +0200

    opensm/osm_pkey_mgr.c: fix segfault when trying to access not allocated block
    If the program decides to add a pkey to block 1. It might not allocate memory
    for block 0. As a result, when the program searches for a pkey, it might try
    access a not allocated block.
    
    Signed-off-by: Daniel Klein <danielk@mellanox.com>
    Signed-off-by: Hal Rosenstock <hal@mellanox.com>
    Signed-off-by: Alex Netes <alexne@mellanox.com>

--- opensm/osm_pkey_mgr.c
+++ opensm/osm_pkey_mgr.c
@@ -556,6 +556,9 @@ static int new_pkey_exists(osm_pkey_tbl_t * p_pkey_tbl, ib_net16_t pkey)
 	num_blocks = (uint16_t) cl_ptr_vector_get_size(&p_pkey_tbl->new_blocks);
 	for (block_index = 0; block_index < num_blocks; block_index++) {
 		block = osm_pkey_tbl_new_block_get(p_pkey_tbl, block_index);
+		if (!block)
+			continue;
+
 		for (pkey_idx = 0; pkey_idx < IB_NUM_PKEY_ELEMENTS_IN_BLOCK;
 		     pkey_idx++) {
 			if (block->pkey_entry[pkey_idx] == pkey)
