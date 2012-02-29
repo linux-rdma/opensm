b3b18617c6d31a25d6c6f7eb3a5ce2d786cab9f7
Mike Heinz <michael.heinz@qlogic.com>
2012-02-29 12:54:16 -0600

    opensm: osm_subnet.c: Updated patch to add error-reporting to the parsing of opensm.conf
    This patch modifies the parse logic as follows:
    
    1. Truncate the line at the first '#' character.
    2. If the result is blank, skip to the next line.
    3. If the first token in the line can be matched against a known token,
       process it as usual.
    4. If the token cannot be matched against a known token, log an error message.
    
    Signed-off-by Mike Heinz <michael.heinz@qlogic.com>
    Signed-off-by Alex Netes <alexne@mellanox.com>

--- opensm/osm_subnet.c
+++ opensm/osm_subnet.c
@@ -1239,9 +1239,10 @@ int osm_subn_parse_conf_file(char *file_name, osm_subn_opt_t * p_opts)
 {
 	char line[1024];
 	FILE *opts_file;
-	char *p_key, *p_val;
+	char *p_key, *p_val, *pound_sign;
 	const opt_rec_t *r;
 	void *p_field1, *p_field2;
+	int token_matched;
 
 	opts_file = fopen(file_name, "r");
 	if (!opts_file) {
@@ -1264,6 +1265,12 @@ int osm_subn_parse_conf_file(char *file_name, osm_subn_opt_t * p_opts)
 	p_opts->file_opts->file_opts = NULL;
 
 	while (fgets(line, 1023, opts_file) != NULL) {
+		pound_sign = strchr(line,'#');
+		token_matched = 0;
+		/* Truncate any comments. */
+		if (pound_sign)
+			*pound_sign = '\0';
+
 		/* get the first token */
 		p_key = strtok_r(line, " \t\n", &p_val);
 		if (!p_key)
@@ -1275,6 +1282,7 @@ int osm_subn_parse_conf_file(char *file_name, osm_subn_opt_t * p_opts)
 			if (strcmp(r->name, p_key))
 				continue;
 
+			token_matched = 1;
 			p_field1 = (void *)p_opts->file_opts + r->opt_offset;
 			p_field2 = (void *)p_opts + r->opt_offset;
 			/* don't call setup function first time */
@@ -1282,6 +1290,9 @@ int osm_subn_parse_conf_file(char *file_name, osm_subn_opt_t * p_opts)
 				    NULL);
 			break;
 		}
+
+		if (!token_matched)
+			log_report(" Unrecognized token: \"%s\"\n", p_key);
 	}
 	fclose(opts_file);
 
@@ -1296,8 +1307,9 @@ int osm_subn_rescan_conf_files(IN osm_subn_t * p_subn)
 	osm_subn_opt_t *p_opts = &p_subn->opt;
 	const opt_rec_t *r;
 	FILE *opts_file;
-	char *p_key, *p_val;
+	char *p_key, *p_val, *pound_sign;
 	void *p_field1, *p_field2;
+	int token_matched;
 
 	if (!p_opts->config_file)
 		return 0;
@@ -1324,6 +1336,13 @@ int osm_subn_rescan_conf_files(IN osm_subn_t * p_subn)
 			      &p_opts->file_opts->qos_rtr_options);
 
 	while (fgets(line, 1023, opts_file) != NULL) {
+		pound_sign = strchr(line,'#');
+		token_matched = 0;
+
+		/* Truncate any comments. */
+		if (pound_sign)
+			*pound_sign = '\0';
+
 		/* get the first token */
 		p_key = strtok_r(line, " \t\n", &p_val);
 		if (!p_key)
@@ -1332,7 +1351,12 @@ int osm_subn_rescan_conf_files(IN osm_subn_t * p_subn)
 		p_val = clean_val(p_val);
 
 		for (r = opt_tbl; r->name; r++) {
-			if (!r->can_update || strcmp(r->name, p_key))
+			if (strcmp(r->name, p_key))
+				continue;
+
+			token_matched = 1;
+
+			if (strcmp(r->name, p_key))
 				continue;
 
 			p_field1 = (void *)p_opts->file_opts + r->opt_offset;
@@ -1341,6 +1365,8 @@ int osm_subn_rescan_conf_files(IN osm_subn_t * p_subn)
 				    r->setup_fn);
 			break;
 		}
+		if (!token_matched)
+                       log_report(" Unrecognized token: \"%s\"\n", p_key);
 	}
 	fclose(opts_file);
 
