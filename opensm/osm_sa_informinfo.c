d95d461bc1a1ac63a84b37bfc2a09ca8aca1e79e
Line Holen <line.holen@oracle.com>
2013-09-03 04:31:23 -0700

    osm_sa_informinfo.c Add attribute info to log messages
    Signed-off-by: Line Holen <line.holen@oracle.com>
    Signed-off-by: Hal Rosenstock <hal@mellanox.com>

--- opensm/osm_sa_informinfo.c
+++ opensm/osm_sa_informinfo.c
@@ -598,7 +598,8 @@ void osm_infr_rcv_process(IN void *context, IN void *data)
 	CL_ASSERT(p_sa_mad->attr_id == IB_MAD_ATTR_INFORM_INFO);
 
 	if (p_sa_mad->method != IB_MAD_METHOD_SET) {
-		OSM_LOG(sa->p_log, OSM_LOG_DEBUG, "Unsupported Method (%s)\n",
+		OSM_LOG(sa->p_log, OSM_LOG_DEBUG,
+			"Unsupported Method (%s) for InformInfo\n",
 			ib_get_sa_method_str(p_sa_mad->method));
 		osm_sa_send_error(sa, p_madw, IB_MAD_STATUS_UNSUP_METHOD_ATTR);
 		goto Exit;
@@ -626,7 +627,8 @@ void osm_infir_rcv_process(IN void *context, IN void *data)
 
 	if (p_sa_mad->method != IB_MAD_METHOD_GET &&
 	    p_sa_mad->method != IB_MAD_METHOD_GETTABLE) {
-		OSM_LOG(sa->p_log, OSM_LOG_DEBUG, "Unsupported Method (%s)\n",
+		OSM_LOG(sa->p_log, OSM_LOG_DEBUG,
+			"Unsupported Method (%s) for InformInfoRecord\n",
 			ib_get_sa_method_str(p_sa_mad->method));
 		osm_sa_send_error(sa, p_madw, IB_MAD_STATUS_UNSUP_METHOD_ATTR);
 		goto Exit;
