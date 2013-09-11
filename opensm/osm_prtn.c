82841323af4cc56535f334a0d0c3af11343e2633
Vladimir Koushnir <vladimirk@mellanox.com>
2013-09-11 12:41:58 +0200

    Use after free in osm_prtn_delete
    Signed-off-by: Vladimir Koushnir <vladimirk@mellanox.com>
    Signed-off-by: Hal Rosenstock <hal@mellanox.com>

--- opensm/osm_prtn.c
+++ opensm/osm_prtn.c
@@ -105,13 +105,13 @@ void osm_prtn_delete(IN osm_subn_t * p_subn, IN OUT osm_prtn_t ** pp_prtn)
 			/* osm_mgrp_cleanup will not delete
 			 * "well_known" groups */
 			p->mgrps[i]->well_known = FALSE;
-			osm_mgrp_cleanup(p_subn, p->mgrps[i]);
 			OSM_LOG(&p_subn->p_osm->log, OSM_LOG_DEBUG,
 				"removing mgroup %s from partition (0x%x)\n",
 				inet_ntop(AF_INET6,
 					  p->mgrps[i]->mcmember_rec.mgid.raw,
 					  gid_str, sizeof gid_str),
 				cl_hton16(p->pkey));
+			osm_mgrp_cleanup(p_subn, p->mgrps[i]);
 		}
 
 		free(p->mgrps);
