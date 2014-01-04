linux-rdma/opensm

commit aaa7b1e67ec6e5fc2a10accf46d538f9d47c6323

osm_db_files.c: Some minor fixes/improvements to osm_db_store
1. Check p_domain_imp->dirty flag under lock
2. Handle malloc failure for temporary file name
3. Move malloc outside of lock

Items 1 and 2 above were:
Pointed-out-by: Bart Van Assche <bvanassche@acm.org>
in http://marc.info/?l=linux-rdma&m=138763427730411&w=2

Signed-off-by: Hal Rosenstock <hal@mellanox.com>

opensm/osm_db_files.c
@@ -475,15 +475,20 @@ int osm_db_store(IN osm_db_domain_t * p_domain,
 
 	p_domain_imp = (osm_db_domain_imp_t *) p_domain->p_domain_imp;
 
-	if (p_domain_imp->dirty == FALSE)
-		goto Exit;
-
 	p_tmp_file_name = malloc(sizeof(char) *
 				 (strlen(p_domain_imp->file_name) + 8));
+	if (!p_tmp_file_name) {
+		OSM_LOG(p_log, OSM_LOG_ERROR, "ERR 6113: "
+			"Failed to allocate memory for temporary file name\n");
+		goto Exit;
+	}
 	strcpy(p_tmp_file_name, p_domain_imp->file_name);
 	strcat(p_tmp_file_name, ".tmp");
 
 	cl_spinlock_acquire(&p_domain_imp->lock);
+
+	if (p_domain_imp->dirty == FALSE)
+		goto Exit;
 
 	/* open up the output file */
 	p_file = fopen(p_tmp_file_name, "w");

commit 86cf679666f49f6073c1ddf2b9ff644a41537a57

osm_db_files.c: Fix issue introduced in commit aaa7b1e67ec6e5fc2a10accf46d538f9d47c6323
osm_db_files.c: Some minor fixes/improvements to osm_db_store

when handling malloc failure, no need to release lock

Signed-off-by: Hal Rosenstock <hal@mellanox.com>

opensm/osm_db_files.c
@@ -480,7 +480,7 @@ int osm_db_store(IN osm_db_domain_t * p_domain,
 	if (!p_tmp_file_name) {
 		OSM_LOG(p_log, OSM_LOG_ERROR, "ERR 6113: "
 			"Failed to allocate memory for temporary file name\n");
-		goto Exit;
+		goto Exit2;
 	}
 	strcpy(p_tmp_file_name, p_domain_imp->file_name);
 	strcat(p_tmp_file_name, ".tmp");
@@ -533,6 +533,7 @@ int osm_db_store(IN osm_db_domain_t * p_domain,
 	p_domain_imp->dirty = FALSE;
 Exit:
 	cl_spinlock_release(&p_domain_imp->lock);
+Exit2:
 	free(p_tmp_file_name);
 	OSM_LOG_EXIT(p_log);
 	return status;