ab9fec9da0dce24f7534f862019e0b0191cde4a1
Hal Rosenstock <hal@mellanox.com>
2014-05-28 23:10:32 +0300

    osm_congestion_control.c: In cc_poller_send, handle cl_event_wait_on return status
    Check for other than success and if so, ignore and continue to wait on event
    
    Signed-off-by: Hal Rosenstock <hal@mellanox.com>

--- opensm/osm_congestion_control.c
+++ opensm/osm_congestion_control.c
@@ -521,16 +521,20 @@ static void cc_poller_send(osm_congestion_control_t *p_cc,
 {
 	osm_subn_opt_t *p_opt = &p_cc->subn->opt;
 	ib_api_status_t status;
+	cl_status_t sts;
 	osm_madw_context_t mad_context = p_madw->context;
 
 	status = osm_vendor_send(p_cc->bind_handle, p_madw, TRUE);
 	if (status == IB_SUCCESS) {
 		cl_atomic_inc(&p_cc->outstanding_mads_on_wire);
 		while (p_cc->outstanding_mads_on_wire >
-		       (int32_t)p_opt->cc_max_outstanding_mads)
-			cl_event_wait_on(&p_cc->sig_mads_on_wire_continue,
-					 EVENT_NO_TIMEOUT,
-					 TRUE);
+		       (int32_t)p_opt->cc_max_outstanding_mads) {
+wait:
+			sts = cl_event_wait_on(&p_cc->sig_mads_on_wire_continue,
+					       EVENT_NO_TIMEOUT, TRUE);
+			if (sts != CL_SUCCESS)
+				goto wait;
+		}
 	} else
 		OSM_LOG(p_cc->log, OSM_LOG_ERROR, "ERR C104: "
 			"send failed to node 0x%" PRIx64 "port %u\n",
