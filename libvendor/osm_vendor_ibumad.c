48d159b7b9f631d6b2e62360bf351e0fe9c2d77c
Hal Rosenstock <hal@mellanox.com>
2014-08-18 18:57:57 +0300

    libvendor/osm_vendor_ibumad.c: Remove GID index 0 check in umad_receiver
    No need for this per C9-46 which states that for UD transport
    service, incoming DGID is only validated if multicast packet.
    
    Signed-off-by: Hal Rosenstock <hal@mellanox.com>

--- libvendor/osm_vendor_ibumad.c
+++ libvendor/osm_vendor_ibumad.c
@@ -327,7 +327,6 @@ static void *umad_receiver(void *p_ptr)
 	osm_mad_addr_t osm_addr;
 	osm_madw_t *p_madw, *p_req_madw;
 	ib_mad_t *p_mad, *p_req_mad;
-	ib_mad_addr_t *p_mad_addr;
 	void *umad = 0;
 	int mad_agent, length;
 
@@ -380,14 +379,6 @@ static void *umad_receiver(void *p_ptr)
 		}
 
 		p_mad = (ib_mad_t *) umad_get_mad(umad);
-		p_mad_addr = umad_get_mad_addr(umad);
-		/* Only support GID index 0 currently */
-		if (p_mad_addr->grh_present && p_mad_addr->gid_index) {
-			OSM_LOG(p_ur->p_log, OSM_LOG_ERROR, "ERR 5409: "
-				"GRH received on GID index %d for mgmt class 0x%x\n",
-				p_mad_addr->gid_index, p_mad->mgmt_class);
-			continue;
-		}
 
 		ib_mad_addr_conv(umad, &osm_addr,
 				 p_mad->mgmt_class == IB_MCLASS_SUBN_LID ||
