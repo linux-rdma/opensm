8d51ae5dabdbe66f8e276ad372c8f4adcada85b2
Daniel Klein <danielk@mellanox.com>
2014-11-20 11:15:11 +0200

    osm_state_mgr.c: Avoid continuing discovery when SM port is unresponsive
    Stop the discovery process if OpenSM fails to query it's own
    local port.
    
    Bug can result in SM crash when searching for it's own GUID.
    It occurs only when the port was successfully discovered on
    the last heavy sweep.
    
    Signed-off-by: Daniel Klein <danielk@mellanox.com>
    Signed-off-by: Hal Rosenstock <hal@mellanox.com>

--- opensm/osm_state_mgr.c
+++ opensm/osm_state_mgr.c
@@ -373,6 +373,13 @@ static boolean_t state_mgr_is_sm_port_down(IN osm_sm_t * sm)
 		state = IB_LINK_ACTIVE;	/* base SP0 */
 	else
 		state = osm_physp_get_port_state(p_physp);
+
+	if (!p_port->discovery_count) {
+		OSM_LOG(sm->p_log, OSM_LOG_ERROR,
+			"ERR 330A: Failed to discover SM port\n");
+		state = IB_LINK_DOWN;
+	}
+
 	CL_PLOCK_RELEASE(sm->p_lock);
 
 Exit:
