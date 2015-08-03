e527ec68f229ce1753ea39208dd13e4e1ccc3684
Hal Rosenstock <hal@mellanox.com>
2015-08-03 22:36:24 +0300

    libvendor/osm_vendor_ibumad_sa.c: Cosmetic formatting changes to osmv_query_sa
    Signed-off-by: Hal Rosenstock <hal@mellanox.com>

--- libvendor/osm_vendor_ibumad_sa.c
+++ libvendor/osm_vendor_ibumad_sa.c
@@ -615,11 +615,11 @@ osmv_query_sa(IN osm_bind_handle_t h_bind,
 		ib_gid_set_default(&u.path_rec.dgid,
 				   ((osmv_guid_pair_t *) (p_query_req->
 							  p_query_input))->
-				   dest_guid);
+							  dest_guid);
 		ib_gid_set_default(&u.path_rec.sgid,
 				   ((osmv_guid_pair_t *) (p_query_req->
 							  p_query_input))->
-				   src_guid);
+							  src_guid);
 		break;
 
 	case OSMV_QUERY_PATH_REC_BY_GIDS:
@@ -632,10 +632,12 @@ osmv_query_sa(IN osm_bind_handle_t h_bind,
 		sa_mad_data.p_attr = &u.path_rec;
 		memcpy(&u.path_rec.dgid,
 		       &((osmv_gid_pair_t *) (p_query_req->p_query_input))->
-		       dest_gid, sizeof(ib_gid_t));
+					      dest_gid,
+		       sizeof(ib_gid_t));
 		memcpy(&u.path_rec.sgid,
 		       &((osmv_gid_pair_t *) (p_query_req->p_query_input))->
-		       src_gid, sizeof(ib_gid_t));
+					      src_gid,
+		       sizeof(ib_gid_t));
 		break;
 
 	case OSMV_QUERY_PATH_REC_BY_LIDS:
@@ -647,8 +649,7 @@ osmv_query_sa(IN osm_bind_handle_t h_bind,
 		    (IB_PR_COMPMASK_DLID | IB_PR_COMPMASK_SLID);
 		sa_mad_data.p_attr = &u.path_rec;
 		u.path_rec.dlid =
-		    ((osmv_lid_pair_t *) (p_query_req->p_query_input))->
-		    dest_lid;
+		    ((osmv_lid_pair_t *) (p_query_req->p_query_input))->dest_lid;
 		u.path_rec.slid =
 		    ((osmv_lid_pair_t *) (p_query_req->p_query_input))->src_lid;
 		break;
