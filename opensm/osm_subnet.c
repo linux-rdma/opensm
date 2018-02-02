deb194fda2190a5553b43a387538141fd24ee668
Hal Rosenstock <hal@mellanox.com>
2018-02-02 15:01:47 +0200

    osm_subnet.c: Add additional ConnectX-5 device ID to is_mlnx_ext_port_info_supported
    Signed-off-by: Hal Rosenstock <hal@mellanox.com>

--- opensm/osm_subnet.c
+++ opensm/osm_subnet.c
@@ -1487,7 +1487,7 @@ int is_mlnx_ext_port_info_supported(ib_net32_t vendid, ib_net16_t devid)
 	if ((devid_ho >= 0xc738 && devid_ho <= 0xc73b) || devid_ho == 0xcb20 ||
 	    devid_ho == 0xcf08 || devid == 0x1b02)
 		return 1;
-	if (devid_ho >= 0x1003 && devid_ho <= 0x1017)
+	if (devid_ho >= 0x1003 && devid_ho <= 0x1019)
 		return 1;
 
 	vendid_ho = cl_ntoh32(vendid);
